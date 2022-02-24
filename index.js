675fcd3f1d433ceb9bd271f8ebb36982dca494e6
John Tarleton Moore <tarletonmoore@gmail.com>
2022-02-24 13:14:59 -0500

    commit tree 4d468593b742f857fdd6c569ca6531aaf2d6991a

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2
+
+
+let random = Math.floor(Math.random() * 10) + 1;
+Number.isInteger(random)
+
+let num3 = 12
+let num4 = 3
+let mod = num3 / num4
+
+let max = Math.max(5, 10, 15, 20)
